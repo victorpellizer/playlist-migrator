6bbc72d4f36776f424fe66af15781ec7a31bb617
Victor Pellizer Iritsu <victor.iritsu@gmail.com>
2025-02-08 23:10:12 -0300

    Friendly readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Just for fun (and because I really need it)
+
+To use:
+1 - create your virtual environment "python -m venv .venv"
+2 - install environment packages "pip install -r requirements.txt"
+3 - open YoutubeMusic (needs login) and Spotify and grab the cookies needed to fill your .env (they can be found at .env.example)
+4 - on main.py fill the your_source_spotify_playlist variable with the url from the Spotify playlist that you want to grab songs from
+5 - on main.py fill the your_destination_ytm_playlist variable with the url from the YoutubeMusic playlist that you want to store your songs at
--- src/main.py
+++ src/main.py
@@ -20,17 +20,11 @@ class PlaylistMigrator:
             destination_app.save_songs_on_pl(song_list=source_app.song_list)
 
 
-pls_spotify = {
-    "awesome_sample": "https://open.spotify.com/playlist/0cSPaVnSIJNoAdZ9dBCtii",
-}
-pls_youtube = {
-    "awesome_sample": "https://music.youtube.com/browse/VLPLYw2qF2LaIHkFnW8vjQoilpSjKWg-sxvg",
-}
+your_source_spotify_playlist="https://open.spotify.com/playlist/0cSPaVnSIJNoAdZ9dBCtii" # awesome sample
+your_destination_ytm_playlist="https://music.youtube.com/browse/VLPLYw2qF2LaIHkFnW8vjQoilpSjKWg-sxvg" # awesome sample
 
-spotify_pl = pls_spotify["awesome_sample"]
-ytm_pl = pls_youtube["awesome_sample"]
-spotify_pl_code = spotify_pl.split("/")[-1]
-ytm_pl_code = ytm_pl.split("/")[-1][2:]
+spotify_pl_code = your_source_spotify_playlist.split("/")[-1]
+ytm_pl_code = your_destination_ytm_playlist.split("/")[-1][2:]
 
 source = App(app=Apps.SPOTIFY, pl_code=spotify_pl_code, token=os.getenv("SPOTIFY_TOKEN"))
 destination = App(app=Apps.YOUTUBE_MUSIC, pl_code=ytm_pl_code, token=os.getenv("YOUTUBE_TOKEN"))
